217ac28b7b4b88b1686d502881b40cf90a61f73f
jhta <jhta@DESKTOP-GD0U3I1>
2023-08-08 17:27:36 +0900

    1일차 사용기능 예상

---
+++ src/main/java/org/homework/Dfunction.java
@@ -0,0 +1,22 @@
+package org.homework;
+
+import java.util.Scanner;
+
+public class Dfunction {
+
+	public static void main(String args[]) {
+        
+		Scanner sc = new Scanner(System.in);
+		String ID = sc.nextLine();
+
+		System.out.println(ID);
+		
+		
+	}
+
+}
+
+
+/*입력, 전체데이터 조회, 해당하는 데이터 삭제 스위치케이스?  */
+
+
